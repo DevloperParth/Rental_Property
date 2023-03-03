ecfb1b6a4d3fad50c8d6a54b98abd7424f7b3399
virendra8180 <virendra.sisodiya@preciousinfosystem.com>
2023-03-03 17:06:14 +0530

    added valodates for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+validates_uniqueness_of :role, :if => lambda {|user| user.role == 'admin' }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   enum :role, { seller: 'sellers', buyer: 'buyers', admin: 'admin' }, prefix: true
@@ -7,4 +9,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
 end
