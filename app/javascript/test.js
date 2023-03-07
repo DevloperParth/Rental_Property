8b054d783601ecce41009c0034e6e6761bf6c68a
virendra8180 <virendra.sisodiya@preciousinfosystem.com>
2023-03-07 19:32:37 +0530

    css and js added properties index

---
+++ app/javascript/test.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+		speed: 400,
+  spaceBetween: 100,
+
+  pagination: '.swiper-pagination1',
+  paginationClickable: true,
+  pagination: '.swiper-pagination2',
+  paginationClickable: true,
+  
+  
+  },
+
+  direction: 'horizontal',
+        slidesPerView: 3,
+        spaceBetween: 10,
+        
+});
+
+
+
+ 
