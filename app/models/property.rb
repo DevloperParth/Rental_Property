ca755e297da92dc749b6f901ca745462ace4f5f6
virendra8180 <virendra.sisodiya@preciousinfosystem.com>
2023-03-07 19:50:21 +0530

    routes changes

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,9 @@
 class Property < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	belongs_to :user
-
+	#scope:  flate -> where{status: flat}
+ #if flate 
+#	Property.flate
+ #elsif RentalProperty
+	#protected
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
    devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  get '/properties/card/action' ,to: "properties#card"
+  get '/properties/flat/action' ,to: "properties#flat"
+  get '/properties/view/action' ,to: "properties#viewpage"
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -14,3 +17,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
+ 
