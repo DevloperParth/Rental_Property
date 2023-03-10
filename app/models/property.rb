5c738f63dc3e4d0f618ddc1e65c13dba73a79f9e
virendra8180 <virendra.sisodiya@preciousinfosystem.com>
2023-03-10 20:10:44 +0530

    property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,9 +1,18 @@
 class Property < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	belongs_to :user
-	#scope:  flate -> where{status: flat}
+
+  has_many :carts
+  has_many :properties, through: :carts
+	#scope:  flat -> where{ property_type: "Plot"}
  #if flate 
 #	Property.flate
  #elsif RentalProperty
 	#protected
+	# byebug
+	# before_initialize :find_flat 
+
+	# def find_flat 
+	# 	@properties = Property.where(property_type: "Plot")
+	# end
 end
